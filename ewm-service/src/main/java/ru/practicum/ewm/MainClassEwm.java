dee54bb085236fffb18cee5adbccc2970d21c2e7
Aleksandr Nikolaev <i@an-98.ru>
2025-09-26 01:29:11 +0300

    fixed errors at Docker

--- ewm-service/src/main/java/ru/practicum/ewm/MainClassEwm.java
+++ ewm-service/src/main/java/ru/practicum/ewm/MainClassEwm.java
@@ -1,11 +1,11 @@
-package ru.practicum.server;
+package ru.practicum.ewm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExploreWithMeServer {
+public class MainClassEwm {
     public static void main(String[] args) {
-        SpringApplication.run(ExploreWithMeServer.class, args);
+        SpringApplication.run(MainClassEwm.class, args);
     }
 }
